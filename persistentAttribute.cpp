199ba957e945b53cdaf17663a965bfcca1753206
Haoming Wang <haoming.exe@gmail.com>
2014-10-29 21:21:42 +0100

    first commit

--- persistentAttribute.cpp
+++ persistentAttribute.cpp
@@ -1,9 +1,9 @@
 #include "persistentAttribute.h"
 
-PersistentAttribute::PersistentAttribute(QString *name, QVariant::Type type, void *data){
-    //this->name = name;
-    this->type = type;
-    this->data = data;
+PersistentAttribute::PersistentAttribute(QString *nom, QVariant::Type typ, void *donnee){
+    name = nom;
+    type = typ;
+    data = donnee;
 }
 
 PersistentAttribute::~PersistentAttribute(){
